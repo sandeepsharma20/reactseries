19132e8c5bd6e4812e356832ea4a8150790329b1
sandeepsharma20 <panditsandeep75051@gmail.com>
2025-05-24 11:19:02 +0530

    save

---
+++ projectone/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Chai from './basic';
+
+function App() {
+  return (
+<Chai/>
+  );
+  
+}
+export default App
