2e7d63bd711da32434e5332ba8cfc1879d5039b3
Your Name <you@example.com>
2025-07-07 22:56:15 +0530

    auth change

---
+++ megablog/src/appwrite/auth.js
@@ -0,0 +1,49 @@
+ import conf from "../conf/conf";
+ import {Client,Account,ID} from "appwrite";
+ export   class AuthService {
+    client =new client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.endpoint)
+        .setProject(conf.appwriteprojectId)
+    }
+    async createAccount({email,password,name}){
+        try{
+await this.account.create( ID.unique(),email,password,name);
+if(userAccount){
+   return this.login({email,password});
+}else {
+     return userAccount;
+}
+        }catch(error){
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password);
+
+        }catch (error){
+            throw error;
+        }
+    }
+    async getCurrentUSer(){
+        try {
+return await this.account.get();
+        }catch(error){
+            console.log("Appwrite service :: getCurrentUSer::error",error);
+        }
+        return null;
+    }
+    async logout(){
+        try{
+await this.account.deleteSessions('current')
+        }catch(error){
+            console.log("")
+        }
+    }
+
+ }
+const AuthService=new AuthService();
+ export default AuthService
---
+++ megablog/src/conf/conf.js
@@ -0,0 +1,10 @@
+ const conf={
+appwriteURL:String(import.meta.env. VITE_APPWRITE_URL),
+appwriteprojectId:String(import.meta.env.VITE_PROJECT_ID),
+appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+appwriteBucketId:string(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+ }
+
+ export default conf
+ //step 2 done here
